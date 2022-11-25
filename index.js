a255d6c5e87884788e0f399aa7e0f59c8fffe51d
Stephen John Siva <stephenjohndsiva3@gmail.com>
2022-11-25 20:14:23 +0800

    I'm sorry Ma'am

---
+++ index.js
@@ -0,0 +1,11 @@
+function htmlLoad(){
+    window.open("https://en.wikipedia.org/wiki/HTML")
+}
+
+function cssLoad(){
+    window.open("https://en.wikipedia.org/wiki/CSS")
+}
+
+function jsLoad(){
+    window.open("https://en.wikipedia.org/wiki/JavaScript")
+}
